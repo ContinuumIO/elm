18e9fe7a8f7bc64fea5e8c72ca2877c8374e4c19
Peter Steinberg <psteinberg@continuum.io>
2016-07-18 10:47:11 -0700

    pass band_specs to load_meta if needed

--- elm/sample_util/band_selection.py
+++ elm/sample_util/band_selection.py
@@ -8,6 +8,7 @@ import re
 
 from elm.config import delayed
 from elm.readers.hdf4 import load_hdf4_array, load_hdf4_meta
+from elm.model_selection.util import get_args_kwargs_defaults
 
 logger = logging.getLogger(__name__)
 
@@ -50,7 +51,11 @@ def _select_from_file_base(filename,
     keep_file = _filename_filter(filename,
                                  search=filename_search,
                                  func=filename_filter)
-    meta = load_meta(filename)
+    args_required, kwargs_default = get_args_kwargs_defaults(load_meta)
+    if len(args_required) == 1:
+        meta = load_meta(filename)
+    else:
+        meta = load_meta(filename, band_specs)
     if metadata_filter is not None:
         keep_file = metadata_filter(filename, meta)
         if not keep_file:
