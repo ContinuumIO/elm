ad04c08dd717a57fe2ce090f2cee7d44830eeb51
Peter Steinberg <psteinberg@continuum.io>
2016-09-29 15:18:13 -0700

    make load_array guess better between HDF4 / 5

--- elm/readers/load_array.py
+++ elm/readers/load_array.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import logging
 import os
 import re
 
@@ -13,28 +14,47 @@ EXT = OrderedDict([
     ('netcdf', ('nc', 'nc\d',)),
     ('hdf5', ('h5', 'hdf5', 'hd5',)),
     ('hdf4', ('hdf4', 'h4', 'hd4',)),
+    ('hdf', ('hdf',))
 ])
 
+logger = logging.getLogger(__name__)
+
 def _find_file_type(filename):
+    '''Guess file type on extension or "tif" if
+    filename is directory, default: netcdf'''
     if os.path.isdir(filename):
         ftype = 'tif'
     else:
-        ext = filename.split('.')[-1]
+        this_ext = filename.split('.')[-1]
         for ftype, exts in EXT.items():
-            if any(re.search(ext, filename, re.IGNORECASE) for ext in exts):
+            if any(re.search(ext, this_ext, re.IGNORECASE) for ext in exts):
                 break
             else:
+
                 ftype = 'netcdf'
     return ftype
 
 
 def load_array(filename, meta=None, band_specs=None):
+    '''Create ElmStore from HDF4 / 5 or NetCDF files or TIF directories
+
+    Parameters:
+
+        filename:   filename (HDF4 / 5 or NetCDF) or
+                    directory name (TIF)
+        meta:       meta data from "filename" already loaded
+        band_specs: list of strings or elm.readers.BandSpec objects
+
+    Returns:
+        es:         ElmStore (xarray.Dataset) with bands specified
+                    by band_specs as DataArrays in "data_vars" attribute
+    '''
     ftype = _find_file_type(filename)
     if meta is None:
         if ftype == 'tif':
-            meta = load_meta(filename, band_specs=band_specs)
+            meta = _load_meta(filename, ftype, band_specs=band_specs)
         else:
-            meta = load_meta(filename)
+            meta = _load_meta(filename, ftype)
     if ftype == 'netcdf':
         return load_netcdf_array(filename, meta, band_specs=band_specs)
     elif ftype == 'hdf5':
@@ -43,10 +63,17 @@ def load_array(filename, meta=None, band_specs=None):
         return load_hdf4_array(filename, meta, band_specs=band_specs)
     elif ftype == 'tif':
         return load_dir_of_tifs_array(filename, meta, band_specs=band_specs)
+    elif ftype == 'hdf':
+        try:
+            es = load_hdf4_array(filename, meta, band_specs=band_specs)
+        except Exception as e:
+            logger.info('NOTE: guessed HDF4 type. Failed: {}. \nTrying HDF5'.format(repr(e)))
+            es = load_hdf5_array(filename, meta, band_specs=band_specs)
+        return es
 
 
-def load_meta(filename, **kwargs):
-    ftype = _find_file_type(filename)
+def _load_meta(filename, ftype, **kwargs):
+
     if ftype == 'netcdf':
         return load_netcdf_meta(filename, **kwargs)
     elif ftype == 'hdf5':
@@ -55,3 +82,27 @@ def load_meta(filename, **kwargs):
         return load_hdf4_meta(filename, **kwargs)
     elif ftype == 'tif':
         return load_dir_of_tifs_meta(filename, **kwargs)
+    elif ftype == 'hdf':
+        try:
+            return load_hdf4_meta(filename, **kwargs)
+        except Exception as e:
+            logger.info('NOTE: guessed HDF4 type. Failed: {}. \nTrying HDF5'.format(repr(e)))
+            return load_hdf5_meta(filename, **kwargs)
+
+
+def load_meta(filename, **kwargs):
+    '''Load metadata for a HDF4 / HDF5 or NetCDF file or TIF directory
+
+    Parameters:
+        filename:       filename (HDF4 / 5 and NetCDF) or directory (TIF)
+        kwargs:         keyword args that may include "band_specs",
+                        a list of string band names or elm.readers.BandSpec
+                        objects
+
+    Returns:
+        meta:           dict with the following keys
+    '''
+
+    ftype = _find_file_type(filename)
+    return _load_meta(filename, ftype, **kwargs)
+
