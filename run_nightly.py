e3bb6af3ef5a7fe18d76e53a87e1d173914c7b31
Greg Brener <noemail@noemail.com>
2016-09-30 10:38:50 -0500

    Add nightly script as a Python file; it used to be Bash. After the CLI needed to get updated, I reused the logic from elm.scripts.run_all_tests after factoring-out the CLI into a separate function.

---
+++ run_nightly.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+import argparse
+import sys
+from elm.scripts import run_all_tests
+
+
+# Reuse the CLI parser from elm.scripts.run_all_tests
+parser = run_all_tests.build_cli_parser(include_required=False)
+args = parser.parse_args()
+
+# Provide the arguments which the user omitted
+if args.dask_clients is None:
+    args.dask_clients = 'SERIAL DISTRIBUTED'
+if args.dask_scheduler is None:
+    args.dask_scheduler = 'localhost:8786'
+if args.remote_git_branch is None:
+    args.remote_git_branch = 'master'
+args.repo_dir = os.getcwd()
+args.elm_configs_path = os.path.join(args.repo_dir, 'example_configs')
+
+
+
+print('PWD = {}'.format(os.getcwd()))
+print('PATH = {}'.format(os.environ.get('PATH')))
+print('dask_clients = {}'.format(args.dask_clients))
+print('dask_scheduler = {}'.format(args.dask_scheduler))
+print('remote_git_branch = {}'.format(args.remote_git_branch))
+print('repo_dir = {}'.format(args.repo_dir))
+print('elm_configs_path = {}'.format(args.elm_configs_path))
+
+
+
+# Run the nightly tests
+start_dt = datetime.datetime.now()
+
+run_all_tests.run_all_tests(args)
+
+end_dt = datetime.datetime.now()
+print('\nSTART TIME: {}'.format(start_dt.isoformat()))
+print('\nEND TIME: {}'.format(end_dt.isoformat()))
+
+
+
