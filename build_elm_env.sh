fd44b02da0867d319b4fc5e1af44348a8664e5de
Peter Steinberg <psteinberg@continuum.io>
2017-05-05 12:45:34 -0700

    fixes to travis.yml to be like earthio setup

---
+++ build_elm_env.sh
@@ -0,0 +1,18 @@
+export ELM_BUILD_DIR=`pwd -P`
+
+# The following two lines could potentially
+# be bypassed by installing earthio, but
+# using build_earthio_env.sh allows the
+# test data to be downloaded as well.
+git clone http://github.com/ContinuumIO/earthio && cd earthio
+. build_earthio_env.sh
+# End of earthio and test data related section
+if [ "$EARTH_VERS" = "" ];then
+    echo FAIL - Must define EARTH_VERS environment variable such as 2.7, 3.5 or 3.6 - FAIL
+else
+    conda remove elm &> /dev/null;
+    pip uninstall -y elm &> /dev/null;
+    conda build conda.recipe --python $EARTH_VERS
+    conda install --use-local elm
+fi
+
