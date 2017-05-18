c6297beb369ed6c26a2fff0029f00c77bf64b695
Peter Steinberg <psteinberg@continuum.io>
2017-05-18 15:58:35 -0700

    fix some travis issues

--- build_elm_env.sh
+++ build_elm_env.sh
@@ -4,18 +4,21 @@ export ELM_BUILD_DIR=`pwd -P`
 # be bypassed by installing earthio, but
 # using build_earthio_env.sh allows the
 # test data to be downloaded as well.
-git clone http://github.com/ContinuumIO/earthio && cd earthio
-. build_earthio_env.sh
-# End of earthio and test data related section
-if [ "$EARTH_VERS" = "" ];then
-    echo FAIL - Must define EARTH_VERS environment variable such as 2.7, 3.5 or 3.6 - FAIL
-else
-    conda update -n root conda;
-    conda remove -n root conda-build;conda install -n root conda-build;
-    conda remove elm &> /dev/null;
-    pip uninstall -y elm &> /dev/null;
-    cd $ELM_BUILD_DIR
-    conda build -c conda-forge conda.recipe --python $EARTH_VERS;
-    conda install -c conda-forge --use-local elm;
-fi
+build_elm_env(){
+    git clone http://github.com/ContinuumIO/earthio && cd earthio
+    . build_earthio_env.sh || return 1;
+    # End of earthio and test data related section
+    if [ "$EARTH_VERS" = "" ];then
+        echo FAIL - Must define EARTH_VERS environment variable such as 2.7, 3.5 or 3.6 - FAIL
+    else
+        conda update -n root conda || return 1;
+        conda remove -n root conda-build;conda install -n root conda-build;
+        conda remove elm &> /dev/null;
+        pip uninstall -y elm &> /dev/null;
+        cd $ELM_BUILD_DIR || return 1;
+        conda build -c conda-forge conda.recipe --python $EARTH_VERS || return 1;;
+        conda install -c conda-forge --use-local elm || return 1;;
+    fi
+}
 
+build_elm_env && source activate $EARTHIO_ENV_TEST && echo OK
