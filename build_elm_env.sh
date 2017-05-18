970a4b398d302d05629f1fffcce98a522660191f
Peter Steinberg <psteinberg@continuum.io>
2017-05-18 16:53:04 -0700

    defaults for env vars in tests

--- build_elm_env.sh
+++ build_elm_env.sh
@@ -5,12 +5,13 @@ export ELM_BUILD_DIR=`pwd -P`
 # using build_earthio_env.sh allows the
 # test data to be downloaded as well.
 build_elm_env(){
-    git clone http://github.com/ContinuumIO/earthio && cd earthio || return 1;
+    git clone http://github.com/ContinuumIO/earthio /tmp/earthio && cd /tmp/earthio || return 1;
     if [ "$EARTHIO_VERSION" = "" ];then
         export EARTHIO_VERSION="master";
     fi
     git checkout $EARTHIO_VERSION || return 1;
     . build_earthio_env.sh || return 1;
+    cd $ELM_BUILD_DIR || return 1;
     # End of earthio and test data related section
     if [ "$PYTHON_TEST_VERSION" = "" ];then
         echo FAIL - Must define PYTHON_TEST_VERSION environment variable such as 2.7, 3.5 or 3.6 - FAIL
@@ -21,7 +22,7 @@ build_elm_env(){
         pip uninstall -y elm &> /dev/null;
         cd $ELM_BUILD_DIR || return 1;
         conda build -c conda-forge conda.recipe --python $PYTHON_TEST_VERSION conda.recipe || return 1;
-        conda install -c conda-forge --use-local elm || return 1;;
+        conda install -c conda-forge --use-local elm || return 1;
     fi
 }
 
