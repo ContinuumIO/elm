0042c43286e67da96ef9a3951ddc090feff7bd34
gbrener <gbrener@continuum.io>
2017-05-24 17:55:06 -0500

    Add support for anaconda-upload

--- build_elm_env.sh
+++ build_elm_env.sh
@@ -18,12 +18,13 @@ build_elm_env(){
     conda config --set always_yes true;
     cd $ELM_BUILD_DIR || return 1;
     # End of earthio and test data related section
-    if [ "$PYTHON_TEST_VERSION" = "" ];then
-        echo FAIL - Must define PYTHON_TEST_VERSION environment variable such as 2.7, 3.5 or 3.6 - FAIL
+    if [ "$PYTHON" = "" ];then
+        echo FAIL - Must define PYTHON environment variable such as 2.7, 3.5 or 3.6 - FAIL
         return 1;
     fi
     conda update -n root conda || return 1;
-    conda remove -n root conda-build;conda install -n root conda-build;
+    conda remove -n root conda-build anaconda-client;
+    conda config --set anaconda_upload no;
     conda remove elm &> /dev/null;
     pip uninstall -y elm &> /dev/null;
     cd $ELM_BUILD_DIR || return 1;
@@ -31,7 +32,7 @@ build_elm_env(){
     conda list || return 1;
     echo conda "env" list is ------
     conda env list || return 1;
-    conda build $EARTHIO_CHANNEL_STR --python $PYTHON_TEST_VERSION conda.recipe || return 1;
+    conda build $EARTHIO_CHANNEL_STR --python $PYTHON conda.recipe || return 1;
     conda install $EARTHIO_CHANNEL_STR --use-local elm || return 1;
 }
 
