aebd959aebb0c9eac013664330446ae7f6af04fe
gbrener <gbrener@continuum.io>
2017-06-08 17:53:59 -0500

    Fix CI builds

--- build_elm_env.sh
+++ build_elm_env.sh
@@ -24,15 +24,14 @@ else
     export PATH="$HOME/miniconda/bin:$PATH"
     source deactivate
     conda env remove -n $EARTHIO_TEST_ENV || true
-    conda env create -n $EARTHIO_TEST_ENV -f environment.yml
-    conda install -n $EARTHIO_TEST_ENV $EARTHIO_CHANNEL_STR -c elm -y python=$PYTHON numpy=$NUMPY earthio
+    conda create -n $EARTHIO_TEST_ENV $EARTHIO_CHANNEL_STR -c elm -y python=$PYTHON numpy=$NUMPY earthio
 fi
-
-source activate $EARTHIO_TEST_ENV
 
 conda config --set always_yes true
 conda config --set anaconda_upload no
 conda install -n root conda conda-build
+
+source activate $EARTHIO_TEST_ENV
 
 conda remove elm &> /dev/null || true
 pip uninstall -y elm &> /dev/null || true
@@ -42,7 +41,7 @@ echo conda "env" list is ------
 conda env list
 
 cd $ELM_BUILD_DIR
-conda build $EARTHIO_CHANNEL_STR --python $PYTHON --numpy $NUMPY conda.recipe
-conda install $EARTHIO_CHANNEL_STR --use-local elm
+conda build $EARTHIO_CHANNEL_STR -c local --python $PYTHON --numpy $NUMPY conda.recipe
+conda install -n $EARTHIO_TEST_ENV $EARTHIO_CHANNEL_STR --use-local elm
 
 set +e
