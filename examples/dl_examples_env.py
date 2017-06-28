7dd7999fa29e52d584d3b464e6914f7ad60a964b
gbrener <gregshipssoftware@gmail.com>
2017-06-28 16:38:41 -0500

    Add "dl_example_env.py" script
    Add a script which downloads datashaders example environment files, so that the environment and data files can be leveraged more easily

---
+++ examples/dl_examples_env.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+
+import sys
+import os
+import requests
+import yaml
+from collections import OrderedDict
+
+
+ENV_SETUP_FILES = {
+    'download_sample_data.py': 'https://raw.githubusercontent.com/bokeh/datashader/master/examples/download_sample_data.py',
+    'datasets.yml': 'https://raw.githubusercontent.com/bokeh/datashader/master/examples/datasets.yml',
+    'environment.yml': 'https://raw.githubusercontent.com/bokeh/datashader/master/examples/environment.yml',
+}
+
+
+def dl_content(dst_fpath, url, tmpdir):
+    """Download content from url and save to dst_path."""
+    with open(os.path.join(tmpdir, dst_fpath), 'wb') as f:
+        resp = requests.get(url)
+        print('Downloading "{}"...'.format(url))
+        f.write(resp.content)
+
+
+def update_env_yml(fpath, tmpdir):
+    """Update environment.yml in tmpdir, and write the new version to current
+    directory.
+    """
+    print('Updating {} for elm...'.format(fpath))
+
+    # Preserve yaml ordering: https://stackoverflow.com/a/21048064
+    _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+    def dict_representer(dumper, data):
+        return dumper.represent_dict(data.items())
+    def dict_constructor(loader, node):
+        return OrderedDict(loader.construct_pairs(node))
+    yaml.add_representer(OrderedDict, dict_representer)
+    yaml.add_constructor(_mapping_tag, dict_constructor)
+
+    with open(os.path.join(tmpdir, fpath), 'r') as fp:
+        env_data = OrderedDict(yaml.safe_load(fp))
+    env_data['name'] = 'elm-examples'
+    env_data['channels'].extend(['elm', 'elm/label/dev', 'numba'])
+
+    deps_to_add = ['earthio', 'elm']
+    deps = env_data['dependencies']
+    found_pip_deps = False
+    for pip_deps_idx, elt in enumerate(deps):
+        if isinstance(elt, dict) and 'pip' in elt:
+            found_pip_deps = True
+            break
+    if found_pip_deps:
+        env_data['dependencies'] = deps[:pip_deps_idx] + deps_to_add + deps[pip_deps_idx:]
+    else:
+        env_data['dependencies'].extend(deps_to_add)
+
+    with open(fpath, 'w') as fp:
+        yaml.dump(env_data, fp, default_flow_style=False)
+
+
+def main(argv):
+    # Download the environment setup files
+    for dst_fpath, url in ENV_SETUP_FILES.items():
+        dl_content(dst_fpath, url, '/tmp')
+
+    # Update the environment.yml for elm's examples
+    assert 'environment.yml' in ENV_SETUP_FILES
+    update_env_yml('environment.yml', '/tmp')
+
+    print('Finished downloading. Next steps:\n'
+          '    $ conda env create -f environment.yml\n'
+          '    $ source activate elm-examples\n'
+          '    $ python download_sample_data.py')
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
