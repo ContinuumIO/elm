ff02dad65145f894655ab0966fa16cfaf848624a
gbrener <gregshipssoftware@gmail.com>
2017-07-20 12:36:50 -0500

    Use hv.Layout to display visualizations
    Also add "-f" option to dup_nldas_nbs.py to overwrite notebooks if desired

--- examples/dup_nldas_nbs.py
+++ examples/dup_nldas_nbs.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import json
 import copy
 import os
+import sys
 from six.moves.urllib.parse import urlparse
 
 urls = [
@@ -33,12 +34,12 @@ first_nb_json = json.load(open(first_nb))
 for url in urls:
     new_nb_name = urlparse(url).path.split(os.sep)[3]
     dest_filename = new_nb_name + '.ipynb'
-    if os.path.isfile(dest_filename):
+    if os.path.isfile(dest_filename) and sys.argv[1] != '-f':
         print('Skipping {} (already exists)...'.format(dest_filename))
-    else:
-        new_nb_json = copy.deepcopy(first_nb_json)
-        new_nb_json['cells'][0]['source'][0] = '# {}\n'.format(new_nb_name)
-        new_nb_json['cells'][3]['source'][0] = "url = '{}'\n".format(url)
-        print('Writing {}...'.format(dest_filename))
-        with open(dest_filename, 'w') as fp:
-            json.dump(new_nb_json, fp)
+        continue
+    new_nb_json = copy.deepcopy(first_nb_json)
+    new_nb_json['cells'][0]['source'][0] = '# {}\n'.format(new_nb_name)
+    new_nb_json['cells'][3]['source'][0] = "url = '{}'\n".format(url)
+    print('Writing {}...'.format(dest_filename))
+    with open(dest_filename, 'w') as fp:
+        json.dump(new_nb_json, fp)
