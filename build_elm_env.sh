b780238b151c26d20b53927d50f4739054eaf6d8
gbrener <gregshipssoftware@gmail.com>
2017-07-26 12:30:45 -0500

    Fix .travis.yml

--- build_elm_env.sh
+++ build_elm_env.sh
@@ -25,7 +25,7 @@ else
     source deactivate
     conda config --set always_yes true
     conda config --set anaconda_upload no
-    conda install -n root conda==4.3.22 conda-build==2.0.4
+    conda install -n root conda conda-build==2.0.4
 
     # Create $EARTHIO_TEST_ENV
     conda env remove -n $EARTHIO_TEST_ENV || true
